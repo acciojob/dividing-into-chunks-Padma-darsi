d8cb5ac77b3625a1e862c59dface2d4f8a6b2971
Padma-darsi <125295009+Padma-darsi@users.noreply.github.com>
2024-11-10 20:18:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,6 +2,30 @@ const arr = [1, 2, 3, 4, 1, 0, 2, 2];
 
 const divide = (arr, n) => {
   // Write your code here
+
+	const result = [];
+  let currentChunk = [];
+  let currentSum = 0;
+
+  for (let num of arr) {
+    if (currentSum + num <= n) {
+      // Add num to the current chunk if it doesn't exceed n
+      currentChunk.push(num);
+      currentSum += num;
+    } else {
+      // Push the current chunk to result and start a new chunk
+      result.push(currentChunk);
+      currentChunk = [num];
+      currentSum = num;
+    }
+  }
+
+  // Push the last chunk if it's not empty
+  if (currentChunk.length) {
+    result.push(currentChunk);
+  }
+
+  return result;
 };
 
 const n = prompt("Enter n: ");
